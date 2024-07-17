81e59fd90b1eaed4ac848e3b4371a066fb267f07
Lucresse Reine ADANDE <adandelucressereine@gmail.com>
2024-07-17 12:00:19 +0100

    Identified Vulnerabilities and Applied Corrective Measures

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const cookieParser = require('cookie-parser');
 const csrf = require('csurf');
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const escapeHTML = require('escape-html');
 
 const app = express();
 const port = 3000;
+const csrfProtection = csrf({cookie: true});
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.set('view engine', 'ejs');
+app.use(csrfProtection);
 
 // Initialize SQLite database
 const db = new sqlite3.Database(':memory:');
@@ -27,8 +31,8 @@ app.get('/', (req, res) => {
 app.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
-    db.get(query, (err, row) => {
+    const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
+    db.get(query, [username, password], (err, row) => {
         if (err) {
             console.error(err);
             res.status(500).send('Internal Server Error');
@@ -43,13 +47,15 @@ app.post('/login', (req, res) => {
 // XSS Vulnerability
 app.get('/xss', (req, res) => {
     const name = req.query.name;
-    res.send(`<h1>Hello, ${name}</h1>`);
+    const escapeName = escapeHTML(name);
+    res.send(`<h1>Hello, ${escapeName}</h1>`);
 });
 
 // CSRF Vulnerability
 app.get('/form', (req, res) => {
     res.send(`
         <form action="/submit" method="POST">
+            <input type="hidden" name="_csrf" value="${req.csrfToken()}" />
             <input type="text" name="data" />
             <button type="submit">Submit</button>
         </form>
@@ -59,6 +65,12 @@ app.get('/form', (req, res) => {
 app.post('/submit', (req, res) => {
     res.send('Form submitted');
 });
+
+app.use((err, req, res, next) => {
+    if (err.code !== 'EBADCSRFTOKEN') return next(err);
+    res.status(403);
+    res.send('Form tampered with');
+}); 
 
 app.listen(port, () => {
     console.log(`App running at http://localhost:${port}`);
