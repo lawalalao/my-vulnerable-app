8b0206cd04446bbf27d3cf31d9d2792ffab68301
Tony Freddy <kounleadissa@gmail.com>
2024-07-17 12:41:18 +0200

    correction csrf bulnerability

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const { escape } = require('querystring');
 
+
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+const csrfProtection = csrf({ cookie: true });
+
 app.set('view engine', 'ejs');
 
 // Initialize SQLite database
@@ -29,7 +33,7 @@ app.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     const query = `SELECT * FROM users WHERE username = ? AND password = ?`;
-    db.get(query, (err, row) => {
+    db.get(query, [username, password], (err, row) => {
         if (err) {
             console.error(err);
             res.status(500).send('Internal Server Error');
@@ -43,23 +47,17 @@ app.post('/login', (req, res) => {
 
 // XSS Vulnerability
 app.get('/xss', (req, res) => {
-    const name = req.query.name || '';
+    const name = req.query.name;
     const encodedName = name.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#39;');
     res.send(`<h1>Hello, ${encodedName}</h1>`);
 });
 
-
 // CSRF Vulnerability
-app.get('/form', (req, res) => {
-    res.send(`
-        <form action="/submit" method="POST">
-            <input type="text" name="data" />
-            <button type="submit">Submit</button>
-        </form>
-    `);
+app.get('/form', csrfProtection, (req, res) => {
+    res.render('form', { csrfToken: req.csrfToken() });
 });
 
-app.post('/submit', (req, res) => {
+app.post('/submit', csrfProtection, (req, res) => {
     res.send('Form submitted');
 });
 
